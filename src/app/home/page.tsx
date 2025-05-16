88e0501745111c6cc815f5207693881650884f13
Daniel Lacerda Xavier <dani.lacerda06@hotmail.com>
2025-05-16 12:04:03 -0300

    correção para o deploy

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from '@/components/header/Header';
 import Footer from '@/components/footer/Footer';
 import Demo from '@/components/demo/Demo';
-import LoginPage from '@/components/login/Login';
 
 export default function Start() {
     return (
